53a915e29540977217fd6349bf15db1b1a09e169
Mathias Ibsen <mathias.ibsen@gmail.com>
2017-10-24 19:56:33 +0200

    Revert "Add ANN, RNN and SVM (#4)" (#5)
    This reverts commit b3e9774e7b4d000aca96fafb694b2c8de3708bca.

--- ML_Ibsen/Models/strategy.py
+++
@@ -1,9 +0,0 @@
-class Strategy:
-
-    def __init__(self, func):
-        if func:
-            self.execute = func
-
-    def execute(self):
-        raise NotImplementedError()
-        # will be overwritten
