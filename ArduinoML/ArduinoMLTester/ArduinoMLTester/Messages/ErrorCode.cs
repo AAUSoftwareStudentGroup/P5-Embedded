cb471ecbe9741137bffdf3a28c6fb3bc71e83548
Morten Rask <morten.rask@live.dk>
2017-11-06 08:58:19 +0100

    Add arduino sketch with embedded NN to identify between Anton and Morten

---
+++ ArduinoML/ArduinoMLTester/ArduinoMLTester/Messages/ErrorCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataHub.Messages
+{
+    public enum ErrorCode
+    {
+        NoError,
+        MissingDataFile,
+        WrongFileType,
+        CouldNotReadFile,
+        InvalidId,
+        NotFound,
+        DataSetNotFound,
+        CouldNotReadData,
+        InvalidLabelId,
+        NoTestsAvailable,
+        NameAlreadyInUse,
+        DataSetInUse,
+        LabelUsedInTests,
+        TestNotFound,
+        MissingModelFile
+    }
+}
